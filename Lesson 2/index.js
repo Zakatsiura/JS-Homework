15e2006ff50af0a016a68664c722ee79e3493900
Zakatsiura <zakatsiura@gmail.com>
2023-02-03 10:02:27 +0200

    final

--- Lesson 2/index.js
+++ Lesson 2/index.js
@@ -110,4 +110,155 @@ let arr1 = [
 
 for(let j=0; j < 10; j++) {
     console.log(arr1[j].password)
+};
+
+// Логічні розгалуження:
+// - Є змінна х, якій ви надаєте довільне числове значення.
+//   Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+let a;
+
+a = 1;
+
+if (a !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+};
+
+a = 0;
+
+if (a !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+};
+
+a = -3;
+
+if (a !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+};
+
+
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+
+let time = 18;
+
+switch (true) {
+    case (time < 15):
+        console.log('First quarter');
+        break;
+    case (time < 30):
+        console.log('Second quarter');
+        break;
+    case (time < 45):
+        console.log('Third quarter');
+        break;
+    default:
+        console.log('Fourth quarter');
+};
+
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
+let day = 18;
+
+switch (true) {
+    case (day < 11):
+        console.log('First part');
+        break;
+    case (day < 21):
+        console.log('Second part');
+        break;
+    default:
+        console.log('Third part');
+};
+
+
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+
+let weekDay = +prompt ('Input day of week by number');
+
+switch (true) {
+    case (weekDay === 1):
+        alert ('Monday');
+        break;
+    case (weekDay === 2):
+        alert ('Tuesday');
+        break;
+    case (weekDay === 3):
+        alert ('Wednesday');
+        break;
+    case (weekDay === 4):
+        alert ('Thurthday');
+        break;
+    case (weekDay === 5):
+        alert ('Friday');
+        break;
+    case (weekDay === 6):
+        alert ('Saturday');
+        break;
+    case (weekDay === 7):
+        alert ('Sunday');
+        break;
+    default:
+        alert ('Incorrect input');
+};
+
+
+
+
+// - Користувач вводить або має два числа.
+//         Потрібно знайти та вивести максимальне число з тих двох .
+//         Також потрібно врахувати коли введені рівні числа.
+
+let number1 = +prompt('Input first number');
+
+let number2 = +prompt('Input second number');
+
+if (typeof(number1) !== Number && typeof(number2) !== Number) {
+    alert('Incorrect input');
+} else {
+    (number1 > number2) ?
+        alert(`Max is ${number1}`) : (number1 < number2) ?
+            alert(`Max is ${number2}`) : alert(`Thay are equal`);
+};
+
+
+// - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+//   за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+
+let y = 0;
+
+// для виключення любого хибноподібного значення:
+
+let x = y || 'Default';
+
+// для виключення лише null та undefined:
+
+let x = y ?? 'Defoult';
+
+console.log(x);
+
+
+
+//
+//
+// - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (k = 0; k < coursesAndDurationArray.length; k++) {
+    if (coursesAndDurationArray[k].monthDuration > 5) {
+        console.log('Super');
+    };
 }
