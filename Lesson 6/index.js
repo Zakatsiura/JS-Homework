7fe8443e3cd3e76cacf022ee9dda5ee910baeea8
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-02-14 23:35:20 +0200

    init

--- Lesson 6/index.js
+++ Lesson 6/index.js
@@ -65,20 +65,35 @@ arr(str);
 
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
 
+let arr1 = [10,8,-7,55,987,-1011,0,1050,0];
 
+const arrToString = (arr1) => {
+    console.log(arr1.map((el) => el.toString()));
+    return arr1.map((el) => el.toString());
+}
 
-
+arrToString(arr1)
 
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-//     let nums = [11,21,3];
+//  let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
 
+let nums = [11,21,3]
 
+function sortNums(nums, direction) {
+    if (direction === 'ascending') {
+        console.log(nums.sort((a, b) => a - b));
+        return nums.sort((a, b) => a - b);
+    } else if (direction === 'descending') {
+        console.log(nums.sort((a, b) => b - a));
+        return nums.sort((a, b) => b - a);
+    }
+}
 
-
-
+sortNums(nums,'ascending');
+sortNums(nums,'descending');
 
 
 // ==========================
@@ -91,14 +106,41 @@ arr(str);
 //     {title: 'FullStack', monthDuration: 7},
 //     {title: 'Frontend', monthDuration: 4}
 // ];
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
 // -- відсортувати його за спаданням за monthDuration
+
+const sortOfDuration = () => {
+        console.log(coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration));
+        return coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+    }
+
+sortOfDuration(coursesAndDurationArray)
+
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+
+const filterOfDuration = () => {
+        console.log(coursesAndDurationArray.filter(el => el.monthDuration > 5));
+        return coursesAndDurationArray.filter(el => el.monthDuration > 5);
+    }
+
+filterOfDuration(coursesAndDurationArray)
+
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 
+const addID = (arr) => {
+    console.log([...arr].map(el => ({ID: '',... el})))
+}
 
-
-
-
+addID(coursesAndDurationArray)
 
 
 
@@ -112,7 +154,7 @@ arr(str);
 //
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
-//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
 //
