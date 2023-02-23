c8763ac862159d0cd5612e05be22ea9eddffe40b
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-02-23 10:46:08 +0200

    init

--- Lesson 8/index.js
+++ Lesson 8/index.js
@@ -1,11 +1,46 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
-// створити пустий масив, наповнити його 10 об'єктами new User(....)
-//
+
+function User(id, name, surname, email, phone) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+}
+
+//   створити пустий масив, наповнити його 10 об'єктами new User(....)
+
+let userArr = [];
+
+for(let i = 1; i < 11; i++) {
+userArr.push(new User(+`${i}`, `name ${i}`, `surname ${i}`, `email@${i}`, +`${i}${i}${i}${i}${i}${i}`))
+}
+console.log(userArr)
+
+
 // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
-//
+
+const userArr1 = userArr.filter(el => (el.id % 2 === 0));
+
+console.log(userArr1);
+
+
+
+
 // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
-//
+
+const userArr2 = userArr.sort((a, b) => (b.id - a.id));
+// Я зробив по спаданню, оскільки масив і так був відсортований по зростанню id. Інакше змінив би умову в sort на (a.id - b.id)
+
+console.log(userArr2);
+
+
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+
+
+
+
+
 // створити пустий масив, наповнити його 10 об'єктами Client
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню.
 // (sort)
