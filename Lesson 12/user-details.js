66fba18c7aaecda852c57708452bd15e3fac0020
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-03-10 18:19:00 +0200

    init

--- Lesson 12/user-details.js
+++ Lesson 12/user-details.js
@@ -5,23 +5,39 @@ const userId = urlParams.get("id");
 
 if (userId) {
     fetch(`http://jsonplaceholder.typicode.com/users/${userId}`)
-        .then(response => response.json())
-        .then(user => {
+        .then((response) => response.json())
+        .then((user) => {
             const userTable = document.createElement("table");
 
-            Object.entries(user).forEach(([key, value]) => {
+            const createTableRow = (key, value) => {
                 const row = document.createElement("tr");
                 const keyCell = document.createElement("td");
                 keyCell.textContent = key;
                 row.appendChild(keyCell);
                 const valueCell = document.createElement("td");
-                valueCell.textContent = value;
+                if (typeof value === "object" && value !== null) {
+                    valueCell.appendChild(createTableFromObject(value));
+                } else {
+                    valueCell.textContent = value;
+                }
                 row.appendChild(valueCell);
+                return row;
+            };
 
+            const createTableFromObject = (object) => {
+                const table = document.createElement("table");
+                Object.entries(object).forEach(([key, value]) => {
+                    table.appendChild(createTableRow(key, value));
+                });
+                return table;
+            };
+
+            Object.entries(user).forEach(([key, value]) => {
+                const row = createTableRow(key, value);
                 userTable.appendChild(row);
             });
 
             userDetails.appendChild(userTable);
         })
-        .catch(error => console.error(error));
+        .catch((error) => console.error(error));
 }
