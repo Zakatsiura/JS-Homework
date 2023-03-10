28a6d357d8537264773fa0550b6305cd27bfd6e0
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-03-10 21:28:53 +0200

    init

--- Lesson 12/index.js
+++ Lesson 12/index.js
@@ -11,8 +11,8 @@ fetch("http://jsonplaceholder.typicode.com/users")
     .then(users => {
         users.forEach(user => {
             const userLink = document.createElement("a");
-            userLink.href = `user-details.html?id=${user.id}`;
             userLink.textContent = `${user.id} - ${user.name}`;
+            userLink.href = `user-details.html?id=${user.id}`;
 
             const userItem = document.createElement("li");
             userItem.appendChild(userLink);
@@ -21,3 +21,52 @@ fetch("http://jsonplaceholder.typicode.com/users")
         });
     })
     .catch(error => console.error(error));
+
+
+
+
+const userDetails = document.getElementById("user-details");
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get("id");
+
+if (userId) {
+    fetch(`http://jsonplaceholder.typicode.com/users/${userId}`)
+        .then((response) => response.json())
+        .then((user) => {
+            const userTable = document.createElement("table");
+
+            const tableRow = (key, value) => {
+                const row = document.createElement("tr");
+                const keyCell = document.createElement("td");
+                keyCell.textContent = `${key} : `;
+                row.appendChild(keyCell);
+                const valueCell = document.createElement("td");
+                if (typeof value === "object" && value !== null) {
+                    valueCell.appendChild(table(value));
+                } else {
+                    valueCell.textContent = value;
+                }
+                row.appendChild(valueCell);
+                return row;
+            };
+
+            const table = (object) => {
+                const table = document.createElement("table");
+                Object.entries(object).forEach(([key, value]) => {
+                    table.appendChild(tableRow(key, value));
+                });
+                return table;
+            };
+
+            Object.entries(user).forEach(([key, value]) => {
+                const row = tableRow(key, value);
+                userTable.appendChild(row);
+            });
+
+            userDetails.appendChild(userTable);
+        })
+        .catch((error) => console.error(error));
+}
+
+
+//   замість файлу users.html в мене index.html
